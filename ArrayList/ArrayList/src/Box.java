684ea401e42299cccefa4b81acb244b627d8ff9c
kulaginaase <kulaginaase@intech.rshb.ru>
2023-01-18 17:27:16 +0300

    Add new folder for autotest

---
+++ ArrayList/ArrayList/src/Box.java
@@ -0,0 +1,54 @@
+public class Box {
+    /**
+     * Создать ArrayList
+     * 1.	Создать ArrayList, содержащий объекты Box. (Объект Box имеет поля вес, высота, широта, глубина)
+     * 2.	Написать метод, который перебирает элементы коллекции и проверяет вес коробок.
+     * 3.	Если вес коробки больше 300 гр, коробка перемещается в другую коллекцию.
+     */
+
+    double weight;
+    double height;
+    double depth;
+    double width;
+
+    public Box(double weight, double height, double depth, double width) {
+        this.weight = weight;
+        this.height = height;
+        this.depth = depth;
+        this.width = width;
+    }
+
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+}
--- ArrayList/ArrayList/src/Main.java
+++ ArrayList/ArrayList/src/Main.java
@@ -1,6 +1,29 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        /**
+         * Создать ArrayList
+         * 1.	Создать ArrayList, содержащий объекты Box. (Объект Box имеет поля вес, высота, широта, глубина)
+         * 2.	Написать метод, который перебирает элементы коллекции и проверяет вес коробок.
+         * 3.	Если вес коробки больше 300 гр, коробка перемещается в другую коллекцию.
+         */
 
-        System.out.println("Hello world!");
+        ArrayList lightBoxes = new ArrayList<Box>();
+        ArrayList hardBoxes = new ArrayList<Box>();
+        Box emptyBox = new Box(1.50, 200, 700, 600);
+        Box hardBox = new Box(300.01, 500, 600, 300);
+        Box inListBox = new Box(299.99, 100, 200, 0);
+        lightBoxes.add(emptyBox);
+        lightBoxes.add(hardBox);
+        lightBoxes.add(inListBox);
+
+//        public void checkWeight() {
+//
+//            }
+//        }
+
+
+
     }
 }
