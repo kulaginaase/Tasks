a26e03ae0159cc0439620b7d29b041888d573ef0
kulaginaase <kulaginaase@intech.rshb.ru>
2023-01-30 16:21:49 +0300

    Add autotest

--- Autotest/src/test/java/tramontana/tramontanaTest.java
+++ Autotest/src/test/java/tramontana/tramontanaTest.java
@@ -1,28 +1,42 @@
 package tramontana;
+
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+
+
+/**
+ * Открыть главную страницу https://tramontana.ru/
+ * Перейти в раздел «Альпинизм»
+ * Перейти в раздел «Оттяжки»
+ * У товарной карточки «УДОЧКА ДЛЯ ВСТЕГИВАНИЯ ОТТЯЖЕК KAILAS CLIP UP III» - проверить прайс.
+ */
 
 public class tramontanaTest {
 
     public static WebDriver driver;
-    Actions builder = new Actions(driver);
     public static WebDriverWait wait;
+    JavascriptExecutor jse = (JavascriptExecutor) driver;
 
     @BeforeClass
     public static void setUp() {
         System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 10);
+        wait = new WebDriverWait(driver, 5);
         driver.navigate().to("https://tramontana.ru/");
+        driver.manage().window().maximize();
     }
 
     @AfterClass
@@ -32,6 +46,31 @@ public class tramontanaTest {
 
     @Test
     public void priceTest() {
-        var price = new By.ByXPath("//html/body/div[2]/main/section/div/div/div[2]/div/div[3]/div/div[18]/div/div[2]/div[3]/span");
+
+        var alpinizm = By.cssSelector("ul.navbar-nav > li:nth-of-type(4)");
+        driver.findElement(alpinizm).click();
+
+        var ottyazhki = driver.findElement(By.cssSelector("div.filter-1>div>div.filter-accordion__item:nth-of-type(10)"));
+        jse.executeScript("arguments[0].scrollIntoView();", ottyazhki);
+        ottyazhki.click();
+
+        var brandButton = By.cssSelector("form.smartfilter > div:nth-of-type(1)");
+        driver.findElement(brandButton).click();
+
+        var kailasCheckBox = By.xpath("//span[@title='Kailas']");
+        driver.findElement(kailasCheckBox).click();
+
+        var productsList = By.cssSelector("div.row>div.col-6:nth-of-type(2)");
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(productsList));
+
+        var productCard = By.cssSelector("a.card__img>img");
+        driver.findElement(productCard).click();
+
+        var priceElement = driver.findElement(By.cssSelector("span.product-card__price"));
+        var exceptedPrice = "4 950 ₽";
+        var actualPrice = priceElement.getText();
+
+        Assert.assertEquals("Стоимость товара не соответствует ожидаемому результату",
+                exceptedPrice, actualPrice);
     }
 }
